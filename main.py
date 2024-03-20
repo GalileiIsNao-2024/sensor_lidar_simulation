a1028789d85f0318a74ca50d54ee70234640abc4
Pietro Peerani <77719826+pietropeerani@users.noreply.github.com>
2024-03-20 09:19:52 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import env, sensors
+import pygame
+
+environment = env.buildEnviroment((1920, 1080))
+environment.originalMap = environment.map.copy()
+
+laser = sensors.laserSensor(200, environment.originalMap, uncertainty=(0.5, 0.01))
+environment.map.fill((0, 0, 0))
+environment.infomap = environment.map.copy()
+
+running = True
+
+while running:
+    sensorON = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if pygame.mouse.get_focused():
+            sensorON = True
+        elif not pygame.mouse.get_focused():
+            sensorON = False
+    if sensorON:
+        position = pygame.mouse.get_pos()
+        laser.position = position
+        sensor_data = laser.sense_obstacles()
+        environment.dataStorage(sensor_data)
+        environment.show_sensorData()
+    environment.map.blit(environment.infomap, (0, 0))
+    pygame.display.update()
